d43b0764406527eb76d68c04c277d1cebb06fbaa
Pavel Pavlov <kx355@mail.ru>
2023-07-01 13:22:45 +0300

    add git icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,27 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    default_component_configs = {
+      icon = {
+        folder_empty = "󰜌",
+        folder_empty_open = "󰜌",
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "✚",
+          deleted = "✖",
+          modified = "",
+          renamed = "󰁕",
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "",
+          staged = "",
+          conflict = "",
+        },
+      },
+    },
     enable_git_status = true,
     enable_diagnostics = true,
     source_selector = {
